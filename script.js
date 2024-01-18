45968c434edfecae15f0963636af4f3f6618bc94
Daniela Almeida <danielaspbrasil@gmail.com>
2024-01-18 13:37:54 -0300

    Inserindo áudios de inicio, pausa e termino

--- script.js
+++ script.js
@@ -80,11 +80,15 @@ inputFocoMusica.addEventListener('change', () => {
 
 
 
+const audioInicia = new Audio('sons/play.wav')
+const audioPausa = new Audio('sons/pause.mp3')
+const audioTempoFinalizado = new Audio('sons/beep.mp3')
 
 const contagemRegressiva = () => {
     if (tempoDecorridoEmSegundos <= 0) {
+        audioTempoFinalizado.play()
+        alert('Tempo finalizado')
         zerar()
-        window.alert('Tempo finalizado')
         return
     }
     tempoDecorridoEmSegundos -= 1
@@ -95,9 +99,11 @@ btnStartPause.addEventListener('click', iniciarOuPausar)
 
 function iniciarOuPausar() {
     if (intervaloId) {
+        audioPausa.play()
         zerar()
         return
     }
+    audioInicia.play()
     intervaloId = setInterval(contagemRegressiva, 1000)
 }
 
