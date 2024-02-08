719e8632ca74cb42ed524f6a54bf616f37c65d9b
Daniela Almeida <danielaspbrasil@gmail.com>
2024-02-08 15:26:11 -0300

    Fechando o formulário ao cancelar tarefa

--- script-crud.js
+++ script-crud.js
@@ -2,6 +2,7 @@ const btnAdicionarTarefa = document.querySelector('.app__button--add-task')
 const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
+const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] 
 
@@ -79,9 +80,10 @@ tarefas.forEach(tarefa => {
 });
 
 
-
-
-
+btnCancelar.onclick = () => {
+    textarea.value = ""
+    formAdicionarTarefa.classList.add('hidden')
+}
 
 
 /*
