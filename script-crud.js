f9c794effaf7bee4c383c16201c3dfd5746a99bb
Daniela Almeida <danielaspbrasil@gmail.com>
2024-02-09 16:12:43 -0300

    Selecionando uma tarefa, deixando-a em andamento e aplicando estilo

--- script-crud.js
+++ script-crud.js
@@ -3,6 +3,7 @@ const formAdicionarTarefa = document.querySelector('.app__form-add-task')
 const textarea = document.querySelector('.app__form-textarea')
 const ulTarefas = document.querySelector('.app__section-task-list')
 const btnCancelar = document.querySelector('.app__form-footer__button--cancel')
+const paragrafoDescricaoTarefa = document.querySelector('.app__section-active-task-description')
 
 const tarefas = JSON.parse(localStorage.getItem('tarefas')) || [] 
 
@@ -50,6 +51,11 @@ function criarElementoTarefa(tarefa) {
     li.append(svg)
     li.append(paragrafo)
     li.append(botaoEditar)
+
+    li.onclick = () => {
+        paragrafoDescricaoTarefa.textContent = tarefa.descricao
+        li.classList.add('app__section-task-list-item-active')
+    }
 
     return li
 }
