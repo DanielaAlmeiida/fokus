50843696958270bcf11bc4d4c4bc40216f17fc88
Daniela Almeida <danielaspbrasil@gmail.com>
2024-02-14 12:59:11 -0300

    Atualizando tarefa concluída no local storage

--- script-crud.js
+++ script-crud.js
@@ -53,24 +53,29 @@ function criarElementoTarefa(tarefa) {
     li.append(paragrafo)
     li.append(botaoEditar)
 
-    li.onclick = () => {
-        document.querySelectorAll('.app__section-task-list-item-active')
-            .forEach(tarefa => {
-                tarefa.classList.remove('app__section-task-list-item-active')
-            })
-
-        if (tarefaSelecionada == tarefa) {
-            paragrafoDescricaoTarefa.textContent = ""
-            tarefaSelecionada = null
-            liTarefaSelecionada = null
-            return
-        }
-
-        tarefaSelecionada = tarefa
-        liTarefaSelecionada = li
-        paragrafoDescricaoTarefa.textContent = tarefa.descricao
-
-        li.classList.add('app__section-task-list-item-active')
+    if (tarefa.completa) {
+        li.classList.add('app__section-task-list-item-complete')
+        botaoEditar.setAttribute('disabled', 'disabled')
+    } else {
+        li.onclick = () => {
+            document.querySelectorAll('.app__section-task-list-item-active')
+                .forEach(tarefa => {
+                    tarefa.classList.remove('app__section-task-list-item-active')
+                })
+    
+            if (tarefaSelecionada == tarefa) {
+                paragrafoDescricaoTarefa.textContent = ""
+                tarefaSelecionada = null
+                liTarefaSelecionada = null
+                return
+            }
+    
+            tarefaSelecionada = tarefa
+            liTarefaSelecionada = li
+            paragrafoDescricaoTarefa.textContent = tarefa.descricao
+    
+            li.classList.add('app__section-task-list-item-active')
+        }    
     }
 
     return li
@@ -113,5 +118,9 @@ document.addEventListener('FocoFinalizado', () => {
         liTarefaSelecionada.classList.remove('app__section-task-list-item-active')
         liTarefaSelecionada.classList.add('app__section-task-list-item-complete')
         liTarefaSelecionada.querySelector('button').setAttribute('disabled', 'disabled')
+
+        tarefaSelecionada.completa = true
+        atualizarTarefas()
     }
 })
+
